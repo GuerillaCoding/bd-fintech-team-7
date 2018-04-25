a3c3cf28ddf8e63bc28d230f0c0848211c91e2c4
Rasmus Larsen <doctorherbstein@gmail.com>
2018-04-25 16:27:58 +0200

    Formatting

--- jquery/js/secret.js
+++ jquery/js/secret.js
@@ -1,47 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
     secret.init();
 });
 
-var secret = (function(){
+var secret = (function () {
     "use strict";
     var secret;
     var inputsequence;
 
-    var _init = function(){
-		secret = [66,65,78,75,68,65,84,65];
+    var _init = function () {
+        secret = [66, 65, 78, 75, 68, 65, 84, 65];
         inputsequence = [];
         bindListeners();
     };
-    
-    var bindListeners = function(){
-        $(document).on('keydown', function(event) {
+
+    var bindListeners = function () {
+        $(document).on('keydown', function (event) {
             if (event.keyCode) {
                 inputsequence.push(event.keyCode);
-          
+
                 if (inputsequence.length > secret.length) {
-                  inputsequence.shift();
+                    inputsequence.shift();
                 }
-          
+
                 if (isFound()) {
-                    service.secret(function(response){
+                    service.secret(function (response) {
                         alert('Cæsar siger: ' + response['response']);
                     });
                 }
             }
-            
+
         });
     };
-    
-    
-    var isFound = function() {
-        if(secret.length != inputsequence.length){
-          return false;
+
+
+    var isFound = function () {
+        if (secret.length != inputsequence.length) {
+            return false;
         }
         return secret.every((element, index) => element === inputsequence[index]);
     }
 
     //public functions
-	return {
-		init : _init
-	}
+    return {
+        init: _init
+    }
 }());
